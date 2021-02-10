b0146283eb917a22030f92bf70406679fab0688f
cyberpunkcoder <cyberpunkcoder@gmail.com>
2021-02-10 14:12:17 -0800

    removed test alien

--- internal/game/game.go
+++ internal/game/game.go
@@ -174,11 +174,13 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		}
 	}
 
-	op.GeoM.Reset()
-	op.GeoM.Translate(-16, -128)
-	g.viewPort.Orient(op)
-	screen.DrawImage(fusionImage, op)
-	screen.DrawImage(alienImage, op)
+	/*
+		op.GeoM.Reset()
+		op.GeoM.Translate(-16, -128)
+		g.viewPort.Orient(op)
+		screen.DrawImage(fusionImage, op)
+		screen.DrawImage(alienImage, op)
+	*/
 
 	// Draw objects according to their layer
 	for layer := 0; layer < len(g.elements); layer++ {

6fc330fe5d311dca9682b597510f9fbe5a5bb711
cyberpunkcoder <cyberpunkcoder@gmail.com>
2021-02-10 14:40:59 -0800

    added updated and draw functions for alien

--- internal/game/alien.go
+++ internal/game/alien.go
@@ -1,11 +1,28 @@
 package game
 
-type alien struct {
+import "github.com/hajimehoshi/ebiten/v2"
+
+// Alien squishy fusion reactor boi
+type Alien struct {
 	Object
 	Character
 }
 
 // CreateAlien at a location
-func CreateAlien(p Position, s Speed) {
+func CreateAlien(p Position, s Speed) Alien {
+	return Alien{Object: Object{
+		Position: p,
+		Speed:    s,
+		Image:    alienImage,
+	}}
+}
+
+// Update the alien state
+func (a *Alien) Update() {
+
+}
+
+// Draw the alien
+func (a *Alien) Draw(screen *ebiten.Image, op *ebiten.DrawImageOptions, g *Game) {
 
 }
